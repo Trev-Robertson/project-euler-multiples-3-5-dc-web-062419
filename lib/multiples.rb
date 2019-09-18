3314d5bf1a23a125d8f44d706adc28ce4528052c
Trevahr Robertson <Trevahr@gmail.com>
2019-09-18 10:14:38 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,34 @@
 # Enter your procedural solution here!
+
+# b = 0
+
+# for a in 1...1000 do 
+#     if a % 3 == 0 || a % 5 == 0 
+#         b += a
+#     end
+# end
+
+# b
+
+def collect_multiples(limit)
+    b = []
+
+    for a in 1...limit do 
+        if a % 3 == 0 || a % 5 == 0 
+            b.push(a)
+        end
+    end
+
+    b
+end
+
+
+def sum_multiples(limit)
+    b = 0
+for a in 1...limit do 
+    if a % 3 == 0 || a % 5 == 0 
+        b += a
+    end
+end
+b
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,31 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+    attr_accessor :limit, :multiples
+  
+    def initialize(limit)
+      @limit = limit
+      @multiples = collect_multiples
+    end
+  
+    def collect_multiples
+      b = []
+
+      for a in 1...limit do 
+          if a % 3 == 0 || a % 5 == 0 
+              b.push(a)
+          end
+      end
+  
+      b
+    end
+  
+    def sum_multiples
+      multiples.inject(:+)
+      b = 0
+      for a in multiples do 
+          b += a
+      end
+      b
+    end
+  end
